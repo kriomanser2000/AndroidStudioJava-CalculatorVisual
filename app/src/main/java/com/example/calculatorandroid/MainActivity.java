9ff6fb05cc6bee75f6a891c0ccf4d02bf281b983
kriomanser2000 <kuzmenkasana05@gmail.com>
2024-12-16 18:41:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculatorandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.calculatorandroid;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.calculator_layout);
+        LinearLayout layout = findViewById(R.id.calcL);
+        layout.setBackgroundColor(getResources().getColor(R.color.BG_color));
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.calcL), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
